a418ee9808e069cd6a595a42d233a16caa3186a6
Mastergamer1309 <123322718+mastergamer1309@users.noreply.github.com>
2023-10-17 21:54:44 -0600

    Update MapScreen.js

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,46 +1,105 @@
-import * as React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as Location from 'expo-location';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker, Polyline } from 'react-native-maps';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
-import {GOOGLE_MAPS_KEY} from "@env";
+import { GOOGLE_MAPS_KEY } from "@env";
 
-const ubicacion_user= require('../assets/image/ubicacion.png')
-
+const ubicacion_user = require('../assets/image/ubicacion.png');
 
 export default function MapScreen() {
-  
-  const [origin, setOrigin] = React.useState({
+  const [origin, setOrigin] = useState({
     latitude: 12.137072,
     longitude: -86.223546,
   });
-  const [destination, setDestination] = React.useState({
+  const [destination, setDestination] = useState({
     latitude: 12.135813,
     longitude: -86.225023,
   });
 
-  React.useEffect(()=>{
-  getLocationPermission();
-  }, [])
+  const [nearbyPharmacies, setNearbyPharmacies] = useState([]);
+  const [showPharmacies, setShowPharmacies] = useState(false);
+
+  const mapRef = useRef(null); 
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
 
   async function getLocationPermission() {
-    let { status } = await Location.requestBackgroundPermissionsAsync();
+    let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted') {
       alert('Permiso de ubicación denegado');
       return;
     }
 
-   let location = await Location.getCurrentPositionAsync({});
-   const current ={
-    latitude: location.coords.latitude,
-    longitude: location.coords.longitude
-   }
-   setOrigin(current);
+    let location = await Location.getCurrentPositionAsync({});
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    };
+    setOrigin(current);
+
+    findNearbyPharmacies(current.latitude, current.longitude);
+  }
+
+  async function findNearbyPharmacies(userLat, userLng) {
+    try {
+      const pharmaciesData = await fetchFarmaciesFromAPI(userLat, userLng);
+
+      setNearbyPharmacies(pharmaciesData);
+      setShowPharmacies(pharmaciesData.length > 0);
+    } catch (error) {
+      console.error('Error al obtener datos de farmacias cercanas', error);
+    }
+  }
+
+  async function fetchFarmaciesFromAPI(userLat, userLng) {
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLng}&radius=5000&type=pharmacy&key=${GOOGLE_MAPS_KEY}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.status === 'OK' && data.results) {
+        return data.results.map(pharmacy => ({
+          latitude: pharmacy.geometry.location.lat,
+          longitude: pharmacy.geometry.location.lng,
+          name: pharmacy.name,
+          vicinity: pharmacy.vicinity
+        }));
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error('Error al obtener datos de farmacias cercanas', error);
+      return [];
+    }
+  }
+
+  async function locateNearestPharmacy() {
+    if (nearbyPharmacies.length > 0) {
+      const nearestPharmacy = nearbyPharmacies[0];
+      setDestination({
+        latitude: nearestPharmacy.latitude,
+        longitude: nearestPharmacy.longitude,
+      });
+
+      const region = {
+        latitude: nearestPharmacy.latitude,
+        longitude: nearestPharmacy.longitude,
+        latitudeDelta: 0.03, 
+        longitudeDelta: 0.03,
+      };
+
+      mapRef.current.animateToRegion(region, 1000); 
+    }
   }
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={{
           latitude: origin.latitude,
@@ -49,26 +108,44 @@ export default function MapScreen() {
           longitudeDelta: 0.04,
         }}
       >
-        <Marker 
-        draggable
-        coordinate = {origin}
-        image={ubicacion_user}
-        
-        onDragEnd={(direction)=> setOrigin(direction.nativeEvent.coordinate)}
+        <Marker
+          draggable
+          coordinate={origin}
+          image={ubicacion_user}
+          onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
         />
         <Marker
-        draggable
-        coordinate = {destination}
-        onDragEnd={(direction)=> setDestination(direction.nativeEvent.coordinate)}
+          draggable
+          coordinate={destination}
+          onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
         />
         <MapViewDirections
-        origin={origin}
-        destination={destination}
-        apikey={GOOGLE_MAPS_KEY}
-        strokeColor="purple"
-        strokeWidth={5}
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_KEY}
+          strokeColor="purple"
+          strokeWidth={5}
         />
-        </MapView>
+        {showPharmacies &&
+          nearbyPharmacies.map((pharmacy, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: pharmacy.latitude,
+                longitude: pharmacy.longitude
+              }}
+              title={pharmacy.name}
+              description={pharmacy.vicinity}
+            />
+          ))
+        }
+      </MapView>
+      <TouchableOpacity
+        style={styles.locateButton}
+        onPress={() => locateNearestPharmacy()}
+      >
+        <Text>Ubicarse en Farmacia Cercana</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -83,5 +160,13 @@ const styles = StyleSheet.create({
   map: {
     width: '90%',
     height: '60%',
+  },
+  locateButton: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
   },
 });
